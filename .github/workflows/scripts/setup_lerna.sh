730bbfcc9aaaac3ca4cc870e6f39de4e53a696f9
Akil Brooks <akil.brooks@gmail.com>
2021-04-13 11:27:24 -0400

    feat(ci): refine publish workflow
    [CARD](https://rentpath.atlassian.net/browse/DX-796)

--- .github/workflows/scripts/setup_lerna.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export GH_ACTOR=$1
-export GH_TOKEN=$2
-git remote set-url origin https://$GH_ACTOR:$GH_TOKEN@github.com/rentpath/cubbyjs
-git config --global user.email "gha_bot@rentpath.com"
-git config --global user.name "GHA Bot"
-git config --global http.http.https://github.com/.extraheader "AUTHORIZATION: bearer ${GH_TOKEN}"
--- .github/workflows/scripts/setup_npmrc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export GH_TOKEN=$1
-echo "//npm.pkg.github.com/:_authToken=${GH_TOKEN}\n@rentpath:registry=https://npm.pkg.github.com/" > .npmrc
-yarn config set @rentpath:registry https://npm.pkg.github.com
